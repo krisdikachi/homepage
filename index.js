4591cfcffef9c000760f97fd266a77281c39bed5
krisdikachi <111499880+krisdikachi@users.noreply.github.com>
2022-12-29 20:19:08 +0100

    seventh change

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ document.getElementById('dropdown').style.display ='none';
 function myFunction(){
     document.getElementById('dropdown').style.display = "block";
 }
+
+
+function myFunction2(){
+    document.getElementById('icon').style.display = "none";
+}
